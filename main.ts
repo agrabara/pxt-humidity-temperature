18c815a323a77bb431824ed42ad487bfcec0bb57
Adam Grabara <44620474+agrabara@users.noreply.github.com>
2020-03-18 18:46:20 +0100

    1 wersja tylko odczyt wilgotności

--- main.ts
+++ main.ts
@@ -1,7 +1,8 @@
-basic.showLeds(`
-    . . . . .
-    . # . # .
-    . . . . .
-    # . . . #
-    . # # # .
-    `);
+basic.showIcon(IconNames.Heart)
+I2C_LCD1602.LcdInit(63)
+I2C_LCD1602.BacklightOn()
+basic.forever(function () {
+    I2C_LCD1602.ShowString("Hum:", 0, 0)
+    I2C_LCD1602.ShowNumber(Environment.temperature(Environment.DHT11Type.DHT11_humidity, DigitalPin.P2), 6, 0)
+    basic.pause(2000)
+})
