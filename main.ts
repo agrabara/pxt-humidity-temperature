01a559e697834214b10c02a13000d03d02ce4852
Adam Grabara <44620474+agrabara@users.noreply.github.com>
2020-03-18 21:33:31 +0100

    Simplest library used for DTH11

--- main.ts
+++ main.ts
@@ -1,4 +1,6 @@
 basic.showIcon(IconNames.Heart)
+
+
 I2C_LCD1602.LcdInit(63)
 I2C_LCD1602.BacklightOff()
 I2C_LCD1602.ShowString("Hello!", 0, 0)
@@ -8,13 +10,14 @@ I2C_LCD1602.ShowString("Humidity &", 0, 0)
 I2C_LCD1602.ShowString("Temperature", 0, 1)
 basic.pause(2000)
 I2C_LCD1602.clear()
+dht11.set_pin(DigitalPin.P2)
 basic.showIcon(IconNames.Yes)
 
 basic.forever(function () {
     I2C_LCD1602.ShowString("Hum %:        ", 0, 0)
-    I2C_LCD1602.ShowNumber(Environment.temperature(Environment.DHT11Type.DHT11_humidity, DigitalPin.P2), 9, 0)
-    basic.pause(1000)
+    I2C_LCD1602.ShowNumber(dht11.humidity(), 9, 0)
+    basic.pause(500)
     I2C_LCD1602.ShowString("Temp C:       ", 0, 1)
-    I2C_LCD1602.ShowNumber(Environment.temperature(Environment.DHT11Type.DHT11_temperature_C, DigitalPin.P2), 9, 1)
+    I2C_LCD1602.ShowNumber(dht11.temperature(), 9, 1)
     basic.pause(2000)
 })
